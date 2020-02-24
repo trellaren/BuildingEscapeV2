0f7d377f3d65e76d1805e48c95f9d6d12c602df5
trellaren <aegis.trellaren@gmail.com>
2020-02-24 08:50:14 -0600

    Update PositionReport.cpp

--- BuildingEscapeV2/Source/BuildingEscapeV2/PositionReport.cpp
+++ BuildingEscapeV2/Source/BuildingEscapeV2/PositionReport.cpp
@@ -6,15 +6,11 @@
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 	UE_LOG(LogTemp, Error, TEXT("The position report script has loaded successfully"));
 	UE_LOG(LogTemp, Error, TEXT("Fuck you, my verbosity is just the right amount"));
 }
 
-
-// Called when the game starts
 void UPositionReport::BeginPlay()
 {
 	FString ObjectName = GetOwner()->GetName();
