33d8e079f4a4cc7b6eb83f491325e42beacc333c
trellaren <aegis.trellaren@gmail.com>
2020-02-24 08:49:35 -0600

    Update OpenDoor.cpp

--- BuildingEscapeV2/Source/BuildingEscapeV2/OpenDoor.cpp
+++ BuildingEscapeV2/Source/BuildingEscapeV2/OpenDoor.cpp
@@ -3,18 +3,13 @@
 
 #include "OpenDoor.h"
 
-// Sets default values for this component's properties
 UOpenDoor::UOpenDoor()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 	UE_LOG(LogTemp, Error, TEXT("The open door script has loaded successfully"));
 	// ...
 }
 
-
-// Called when the game starts
 void UOpenDoor::BeginPlay()
 {
 	AActor* Owner = GetOwner();
